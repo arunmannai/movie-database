2c034d97ebe3066d36f11f6b1a4981b933e1cc6e
Arun Baskaran <arunmannai@gmail.com>
2024-06-04 15:35:08 +0530

    iframe fixes

--- components/youtubevideo.js
+++ components/youtubevideo.js
@@ -1,5 +1,5 @@
 function YoutubeVideo(props) {
-  return <iframe width="560" height="315" src={props.url} title={props.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    
+  return <iframe width="560" height="315" src={props.url} title={props.title} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 }
+
 export default YoutubeVideo;
