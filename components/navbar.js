9de89d152d80c8cf859b4d7e1e5b59b504026b49
Arun Baskaran <arunmannai@gmail.com>
2024-06-04 15:06:47 +0530

    collabsible navbar for mobile

--- components/navbar.js
+++ components/navbar.js
@@ -1,26 +1,34 @@
 import Link from "next/link";
+import { useState } from "react";
 
 function Navbar() {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggle = () => setCollapsed(!collapsed);
+  const collapse = () => setCollapsed(true);
+
   return (
     <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
       <div className="container-fluid">
-        <div class="container-fluid">
-          <Link class="navbar-brand" href="/">
-            Movies DB
-          </Link>
+        <Link className="navbar-brand" href="/" onClick={collapse}>
+          Movies DB
+        </Link>
+        <button className="navbar-toggler" type="button" onClick={toggle}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`navbar-collapse ${collapsed && 'collapse'}`}>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" href="/movies" onClick={collapse}>
+                Movies
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/series" onClick={collapse}>
+                Series
+              </Link>
+            </li>
+          </ul>
         </div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" href="/movies">
-              Movies
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="/series">
-              Series
-            </Link>
-          </li>
-        </ul>
       </div>
     </nav>
   );
