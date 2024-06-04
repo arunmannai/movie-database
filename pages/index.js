a609fe5c3a79eefd61091536e17799f56416294c
Arun Baskaran <arunmannai@gmail.com>
2024-06-04 14:08:32 +0530

    Home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -10,7 +12,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Hello World</h1>
+        <div className="container-fluid p-5 bg-primary text-white text-center">
+          <h1>Top 100 Movies and Series</h1>
+        </div>
+        <div className="container mt-5">
+          <div className="row">
+            <div className="text-center itemcontainer category m-3 py-5" onClick={() => router.push('/movies')}>
+              <h3>Movies</h3>
+              <p>Click here to view top 100 movies</p>
+            </div>
+            <div className="text-center itemcontainer category m-3 py-5" onClick={() => router.push('/series')}>
+              <h3>Series</h3>
+              <p>Click here to view top 100 series</p>
+            </div>
+          </div>
+          <div className="row mt-3">
+            <div className="text-center">Pick any one to continue...</div>
+          </div>
+        </div>
       </main>
     </>
   );
